73f7905f768cdcda54cc21ff591449a4651d68a0
JEREMIE NERET <jeremie.neret@gmail.com>
2025-04-14 13:26:49 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Background from './components/Background';
+import './style/CSS/style.css';
+
+export default function App() {
+  return (
+    <>
+      <Background />
+    </>
+  )
+}
---
+++ src/components/Background.jsx
@@ -0,0 +1,9 @@
+export default function Background() {
+  return (
+    <>
+      <div className="background woodsmoke">
+        <div className="background-img"></div>
+      </div>
+    </>
+  )
+}
